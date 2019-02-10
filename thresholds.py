cce9c633749c31d3f389cc5066a5c2e104ff4e12
lucatraini <lucatra89@gmail.com>
2019-02-11 00:17:08 +0100

    Added another KMean-based selector, with different selection criteris (best F-measure)

--- thresholds.py
+++ thresholds.py
@@ -30,6 +30,8 @@ class Normalizer:
 class Selector(ABC):
     def __init__(self, normalizedTrace, backends, frontend, frontendSLA):
         self.backends = backends
+        self.p = normalizedTrace.filter(col(frontend)> frontendSLA).count()
+        self.n = normalizedTrace.filter(col(frontend)<= frontendSLA).count()
         self.thresholdsDict = {}
         self.tprDict = {}
         self.fprDict = {}
@@ -51,6 +53,36 @@ class Selector(ABC):
 
 
 class KMeansSelector(Selector):
+    def computeFMeasure(self, tpr, fpr):
+        tp = tpr*self.p
+        fp = fpr*self.n
+        recall = tp/self.p
+        precision = tp/(tp+fp) if tp > 0 or fp>0 else 0
+        return (2*(precision*recall)/(precision+recall)
+                if precision > 0 or recall > 0
+                else 0)
+
+    def select(self, k):
+        thresholdDict = {}
+        for aBackend in self.backends:
+            thresholds = self.thresholdsDict[aBackend]
+            if k + 1 >= (len(thresholds)):
+                thresholdDict[aBackend] = thresholds
+                continue
+            tpr = self.tprDict[aBackend]
+            fpr = self.fprDict[aBackend]
+            X = list(zip(tpr, fpr))
+            kmeans = KMeans(n_clusters=k, random_state=0).fit(X)
+            res = []
+            for i in range(k):
+                indexes = [j for j in np.where(kmeans.labels_== i)[0]]
+                bestIndex = max(indexes, key= lambda j: self.computeFMeasure(tpr[j], fpr[j]))
+                res.append(thresholds[bestIndex])
+            thresholdDict[aBackend] = [thresholds[0]] + sorted(res)
+        return thresholdDict
+
+
+class KMeansSelector2(Selector):
     def select(self, k):
         thresholdDict = {}
         for aBackend in self.backends:
@@ -66,7 +98,7 @@ class KMeansSelector(Selector):
             thresholdDict[aBackend] = [thresholds[0]] + sorted(thresholds[i + 1] for i in indices)
         return thresholdDict
 
-
+    
 class RandSelector(Selector):
     def select(self, k):
         thresholdDict = {}
