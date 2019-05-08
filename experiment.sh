ef9e0034ec798d440a14f349e1bde71504323427
lucatraini <lucatra89@gmail.com>
2019-05-08 16:48:19 +0200

    Added PYTHONPATH comment

--- experiment.sh
+++ experiment.sh
@@ -3,9 +3,9 @@ dir=$PWD
 source /home/luca/environments/spark/bin/activate
 cd /home/luca/cococcia-shop
 
-for i in $(seq 2 $1)
+for i in $(seq 2 5)
 do
-    for j in $(seq 1 $2)
+    for j in $(seq 1 10)
     do
         docker-compose down
         docker-compose -f docker-compose.tracing.yml stop zipkin
@@ -25,3 +25,5 @@ do
         echo $i';'$t1';'$t2  >> $dir/experiments.csv
     done
 done
+
+#export PYTHONPATH=$SPARK_HOME/python
