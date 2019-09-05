c971b6a0cbe16a48f3345d473a4fd5a291a0eb9d
lucatraini <lucatra89@gmail.com>
2019-09-05 16:07:04 +0200

    Refactorirng range analysis

--- ks/range_analysis.py
+++ ks/range_analysis.py
@@ -3,34 +3,18 @@ from ks.branchandbound import BranchAndBound
 
 
 class RangeAnalysis:
-    def __init__(self, traces, backends,
-                 frontend, splitPoints):
-        self.traces = traces
-        self.backends = backends
-        self.frontend = frontend
+    def __init__(self, explainInterval, splitPoints):
         self.splitPoints = splitPoints
+        self.explainInterval = explainInterval
 
-    def _explainSingleIntervalGA(self, from_, to, thresholds_dict):
-        ga = GA(self.traces,
-                self.backends,
-                self.frontend,
-                thresholds_dict)
-        return ga.compute(from_, to)
-
-    def _explainSingleIntervalBnB(self, from_, to, thresholds_dict):
-        bnb = BranchAndBound(self.traces,
-                             self.frontend,
-                             thresholds_dict)
-        return bnb.compute(from_, to)
-
-    def _computeBestSplits(self, i, f):
+    def _computeBestSplits(self, i):
         if i == 0:
             return [], 0, ()
         else:
             bestSelectedSplits, bestSumFMeas, bestExplanations = None, None, None
             for j, v in enumerate(self.splitPoints[:i]):
-                selectedSplits, sumFmeas, fmeasures = self._computeBestSplits(j, f)
-                exp = f(self.splitPoints[j], self.splitPoints[i])
+                selectedSplits, sumFmeas, fmeasures = self._computeBestSplits(j)
+                exp = self.explainInterval(self.splitPoints[j], self.splitPoints[i])
                 fmeas = exp[1]
                 sumFmeas += fmeas
                 if bestSumFMeas is None or bestSumFMeas < sumFmeas:
@@ -40,12 +24,7 @@ class RangeAnalysis:
 
             return bestSelectedSplits, bestSumFMeas, bestExplanations
 
-    def explainsWithGA(self, thresholds_dict,  bandwidth=10):
+    def explain(self):
         n = len(self.splitPoints) - 1
-        f = lambda from_, to: self._explainSingleIntervalGA(from_, to, thresholds_dict,  bandwidth)
-        return self._computeBestSplits(n, f)
+        return self._computeBestSplits(n)
 
-    def explainsWithBnB(self, thresholds_dict):
-        n = len(self.splitPoints) - 1
-        f = lambda from_, to: self._explainSingleIntervalBnB(from_, to, thresholds_dict)
-        return self._computeBestSplits(n, f)
