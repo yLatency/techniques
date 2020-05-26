2c52c0f1af2fbad64f45beae291a49efd994606a
lucatraini <lucatra89@gmail.com>
2020-05-26 10:17:15 +0200

    Genetic range analysis algorithm now run in parallel by default

--- ylatency/gra.py
+++ ylatency/gra.py
@@ -1,4 +1,8 @@
 import copy
+from math import ceil
+from multiprocessing.pool import Pool
+from multiprocessing import get_context
+from os import cpu_count
 
 from deap import creator, base, tools, algorithms
 
@@ -7,42 +11,68 @@ from ylatency.grautils import FitnessUtils
 from ylatency.thresholds import Hashtable
 
 
+class ParallelHelper:
+    @staticmethod
+    def make_global(_evaluate):
+        global evaluate
+        evaluate = _evaluate
+        creator.create("Fitness", base.Fitness, weights=(1.0, 1.0, -1.0))
+        creator.create("Individual", list, fitness=creator.Fitness)
+
+
+    @staticmethod
+    def evaluate(ind):
+        return evaluate(ind)
+
+
 class GeneticRangeAnalysis:
     def __init__(self, traces, target_col, thresholds_dict, min_, max_):
         columns = list(thresholds_dict.keys())
         ht = Hashtable(traces, columns, target_col, min_, max_)
         self._ops = Operator(thresholds_dict)
         self._fitness = FitnessUtils(columns, ht.positives(thresholds_dict), ht.negatives(thresholds_dict))
+        self.toolbox = base.Toolbox()
+        self._initga()
 
-    def _initga(self, toolbox):
+    def _initga(self):
+        creator.create("Fitness", base.Fitness, weights=(1.0, 1.0, -1.0))
         creator.create("Individual", list, fitness=creator.Fitness)
-        toolbox.register("individual", tools.initIterate, creator.Individual, self._ops.expllist)
-        toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+        self.toolbox.register("individual", tools.initIterate, creator.Individual, self._ops.expllist)
+        self.toolbox.register("population", tools.initRepeat, list, self.toolbox.individual)
 
-        toolbox.register("mate", self._ops.cx)
-        toolbox.register("mutate", self._ops.mut)
+        self.toolbox.register("mate", self._ops.cx)
+        self.toolbox.register("mutate", self._ops.mut)
+        self.toolbox.register("select", tools.selNSGA2)
 
-    def explain(self, mu=100, lambda_=100, ngen=400, cp=0.6, mut=0.4, stats=False):
-        toolbox = base.Toolbox()
-        creator.create("Fitness", base.Fitness, weights=(1.0, -1.0))
-        self._initga(toolbox)
-        toolbox.register("select", tools.selNSGA2)
-        f = self._fitness
-        toolbox.register("evaluate", lambda ind: (f.fscore(ind), f.dissimilarity(ind)))
-        toolbox.decorate("evaluate", tools.DeltaPenalty(f.feasible, (0, float('inf'))))
+    def _explain_sequential(self, cp, lambda_, mu, mut, ngen, stats, verbose):
+        self.toolbox.register("evaluate", self._fitness.evaluate)
+        return self._explain(cp, lambda_, mu, mut, ngen, stats, verbose)
 
+    def _explain_parallel(self, cp, lambda_, mu, mut, ngen, stats, verbose):
+        processes_ = min(ceil(lambda_/5), cpu_count())
+        with get_context("spawn").Pool(processes_, initializer=ParallelHelper.make_global, initargs=(self._fitness.evaluate, )) as pool:
+            self.toolbox.register("evaluate", ParallelHelper.evaluate)
+            self.toolbox.register("map", pool.map)
+            return self._explain(cp, lambda_, mu, mut, ngen, stats, verbose)
+
+    def _explain(self, cp, lambda_, mu, mut, ngen, stats, verbose):
         self._ops.mut_prob = mut
-
         if stats:
             stats = tools.Statistics()
             stats.register("pop", copy.deepcopy)
         else:
             stats = None
-
-        pop = toolbox.population(n=ngen)
-        res, logbook = algorithms.eaMuPlusLambda(pop, toolbox,
+        hof = tools.ParetoFront(lambda ind1, ind2: ind1.fitness.values == ind1.fitness.values)
+        pop = self.toolbox.population(n=ngen)
+        res, logbook = algorithms.eaMuPlusLambda(pop, self.toolbox,
                                                  mu=mu,
                                                  lambda_=lambda_,
                                                  cxpb=cp, mutpb=mut, ngen=ngen,
-                                                 stats=stats, verbose=False)
-        return res, logbook
+                                                 stats=stats, halloffame=hof, verbose=verbose)
+        return hof, logbook
+
+
+    def explain(self, mu=30, lambda_=30, ngen=300, cp=0.6, mut=0.4, stats=False, verbose=False, parallel=True):
+        explain_ = self._explain_parallel if parallel else self._explain_sequential
+        hof, logbook = explain_(cp, lambda_, mu, mut, ngen, stats, verbose)
+        return hof, logbook
